976b00d123053c09dd3f980419bd92d5bdb72470
fislam <faizulcse@gmail.com>
2021-08-26 16:01:24 +0600

    validate input data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,47 @@
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    static Scanner scanner;
-
     public static void main(String[] args) {
-        System.out.println("Enter student number Array List: ");
-        scanner = new Scanner(System.in);
+        System.out.println("Enter student data: ");
+        Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
+        /*
+         * Scanner.nextInt method does not read the newline character.
+         * To skip newline character add an optional scanner.nextLine()
+         */
+        scanner.nextLine();
+
         List<Student> studentList = new ArrayList();
-
         for (int i = 0; i < size; i++) {
+            String input;
+            String data[];
+            input = scanner.nextLine();
+            data = input.split("\\s");
+
+            int id;
+            String name;
+            double cgpa;
+            id = Integer.parseInt(data[0].trim().replaceFirst("^0*", ""));
+            name = data[1].trim().toLowerCase();
+            cgpa = Double.parseDouble(new DecimalFormat("#.##").format(Double.parseDouble(data[2].trim())));
+
             Student student = new Student();
-            student.setId(scanner.nextInt());
-            scanner.nextLine();
-            student.setFirstName(scanner.nextLine().toLowerCase().trim());
-            student.setCgpa(Double.parseDouble(new DecimalFormat("##.##").format(scanner.nextDouble())));
-            studentList.add(student);
+            student.setId(id);
+            student.setFirstName(name);
+            student.setCgpa(cgpa);
+            studentList.add(i, student);
         }
+//        Collections.sort(studentList, new CgpaComparator());
+//        Collections.sort(studentList, new IdComparator());
+//        Collections.sort(studentList, new NameComparator());
 
-        Collections.sort(studentList, new CgpaComparator());
-        Collections.sort(studentList, new IdComparator());
-        Collections.sort(studentList, new NameComparator());
-
-        for (Student student : studentList) {
-            System.out.println(student.getFirstName());
+        for (Student stu : studentList) {
+            System.out.println(stu);
         }
     }
 }
+
